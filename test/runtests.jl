eccc258e17ae29f1b46d3ea704ab25fa41ae359a
SimonDanisch <sdanisch@gmail.com>
2017-04-14 20:14:45 +0200

    test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,28 @@
 using SPIRV
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# we use CUDAnative to handle all the GPU codegen modification to Julias codegen
+using CUDAnative, LLVM
+
+function kernel_vadd(a, b, c)
+    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    c[i] = a[i] + b[i]
+    return nothing
+end
+
+const CUArray = CUDAnative.CuDeviceArray{Float32, 1}
+
+ir = sprint() do io
+    CUDAnative.code_llvm(io, kernel_vadd, Tuple{CUArray, CUArray, CUArray}; dump_module = true, cap = v"2.0")
+end
+mod = parse(LLVM.Module, ir)
+
+spirv_dir = joinpath(homedir(), "gpustuff", "SPIRV-LLVM", "build", "bin")
+llvm_spirv = joinpath(spirv_dir, "llvm-spirv")
+llvm_dis = joinpath(spirv_dir, "llvm-dis")
+@assert isfile(llvm_spirv)
+
+open(`$llvm_dis -`, "w") do f
+    buff = convert(Vector{UInt8}, mod)
+    write(f, buff)
+end
